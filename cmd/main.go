7a05fc03cce793d3e37d1493f5d54fff2bc96176
Ruslan Semak <ruslan.semack1501@gmail.com>
2022-06-24 22:37:12 +0300

    feat(app): base DI

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	handler "learning_api/internal/gateway/http"
+	"learning_api/internal/repository"
+	"learning_api/internal/service"
 	todo "learning_api/pkg"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run(8000, handlers.InitRoutes()); err != nil {
