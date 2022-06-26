fb1ae20bbbfa4b444bce0165da1c3e8a5359f806
Ruslan Semak <ruslan.semack1501@gmail.com>
2022-06-26 21:23:45 +0300

    feat(db): compose file and init migration

---
+++ migrations/20220626210819_init.sql
@@ -0,0 +1,42 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users
+(
+    id            serial       not null unique,
+    name          varchar(255) not null,
+    username      varchar(255) not null unique,
+    password_hash varchar(255) not null
+
+);
+
+CREATE TABLE todo_lists
+(
+    id          serial       not null unique,
+    title       varchar(255) not null,
+    description varchar(255)
+);
+
+CREATE TABLE users_lists
+(
+    id       serial                                           not null unique,
+    user_id  int references users (id) on delete cascade      not null,
+    list__id int references todo_lists (id) on delete cascade not null
+
+);
+
+CREATE TABLE todo_items
+(
+    id          serial       not null unique,
+    title       varchar(255) not null,
+    description varchar(255),
+    done        boolean     not null default false
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table todo_items;
+drop table users_lists;
+drop table todo_lists;
+drop table users;
+-- +goose StatementEnd
