bf0a1056e0dafa27bf868110ab8396a8d1bdb394
Ruslan Semak <ruslan.semack1501@gmail.com>
2022-06-27 19:22:55 +0300

    feat(users): authentication

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -3,12 +3,22 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/sirupsen/logrus"
 	"learning_api/internal/entity"
 	"learning_api/internal/repository"
+	"os"
+	"strconv"
+	"time"
 )
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -18,6 +28,27 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user entity.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(_user entity.UserLogin) (string, error) {
+	user, err := s.repo.GetUser(_user.Username, generatePasswordHash(_user.Password))
+	if err != nil {
+		return "", err
+	}
+
+	ttl, err := strconv.ParseInt(os.Getenv("JWT_TOKEN_TTL_HOUR"), 10, 64)
+
+	if err != nil {
+		logrus.Fatal("JWT_TOKEN_TTL_HOUR env must be integer: %s", err.Error())
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Duration(ttl) * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		}, user.Id})
+
+	return token.SignedString([]byte(os.Getenv("JWT_TOKEN_KEY")))
 }
 
 func generatePasswordHash(password string) string {
